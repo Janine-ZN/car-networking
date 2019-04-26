938c56b1da80d229975d3b757873334399d5afe6
Janine-ZN <Janine_z@163.com>
2019-04-26 15:08:19 +0800

    返回到顶部的图标初次打开页面应该被隐藏

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
 $(function () {
-
-    //返回顶部===>出现与消失
-    //当屏幕滚动，触生 scroll 事件
+    // 返回顶部 ===> 出现与消失
+    // 当屏幕滚动，触生 scroll 事件
     $(window).scroll(function () {
-
         var top1 = $(this).scrollTop(); //获取相对滚动条顶部的偏移
-
         if (top1 >200) { // 当偏移大于450px时让图标淡入（css设置为隐藏）
-
             $(".top").stop().fadeIn();
-
         } else {
             //当偏移小于200px时让图标淡出
             $(".top").stop().fadeOut();
@@ -22,5 +17,4 @@ $(function () {
             scrollTop: 0
         }, 300); //300是所用时间
     });
-
 });
